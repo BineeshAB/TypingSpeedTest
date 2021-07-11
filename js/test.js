481c9f3a50a14b35c8feeada448c9a7fb17696d0
Bineesh Alakkal <31299407+BineeshAB@users.noreply.github.com>
2021-07-11 12:43:02 +0530

    Updated Typing test website

---
+++ js/test.js
@@ -0,0 +1,110 @@
+const setOfWords = [
+    "The five boxing wizards jump quickly.\n",
+    "Quick fox jumps nightly above wizard.\n",
+    "Fox dwarves chop my talking quiz job.\n",
+    "Pack my box with five dozen liquor jugs.\n",
+    "When zombies arrive, quickly fax judge pat.\n",
+    "The quick brown fox jumps over the lazy dog.\n",
+    "Jacky can now give six big tips from the old quiz.\n",
+    "All questions asked by five watched experts amaze the judge.\n"
+];
+const words  = document.getElementById("question");
+const typedWords = document.getElementById("message");
+const btn = document.getElementById('btn');
+let startTime, endTime, errorWords, speed;
+
+typedWords.addEventListener('input', ()=>{
+    const arryInput = words.querySelectorAll('span');
+    const arryValue = typedWords.value.split('');
+    arryInput.forEach((characterSpan, index) => {
+        const character = arryValue[index];
+        if(character == null){
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+        }
+        else if(character == characterSpan.innerText){
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        }
+        else {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+        }
+    });
+});
+
+const startTest = () => {
+    let randomNumber = Math.floor(Math.random()*setOfWords.length);
+    const quote = setOfWords[randomNumber];
+    words.innerText = '';
+    let date = new Date;
+    startTime = date.getTime();
+    btn.value = "Done";
+    typedWords.disabled = false;
+
+    quote.split('').forEach(character =>{
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        words.appendChild(characterSpan)
+    });
+}
+
+const endTest = () => {
+    let date = new Date();
+    endTime = date.getTime();
+    let totalTime = ((endTime - startTime)/1000);
+    btn.value ="Start";
+
+    let totalStr = typedWords.value;
+    let wordCount = wordCounter(totalStr);
+
+    speed = Math.round((wordCount/totalTime)*60);
+
+    let finalMsg = "Your speed is " + speed + " words per mintues.\n";
+    finalMsg += compareWords(words.innerText, totalStr);
+    words.innerText = finalMsg;
+
+    typedWords.value = typedWords.defaultValue;
+    typedWords.disabled = true;
+}
+
+const compareWords = (str1, str2) =>{
+    let word1 = str1.split(" ");
+    let word2 = str2.split(" ");
+    let count = 0;
+
+    word1.forEach(function(item, index){
+        if (item == word2[index]) {
+            count++;
+        }
+    })
+
+    errorWords = (word1.length - count);
+
+    let avg = speed - errorWords;
+
+    return(errorWords + " error out of " + word1.length + " words. And so your avarage speed is " + avg + " words per mintues.");
+}
+
+const wordCounter = (str) =>{
+    let  response = str.split(" ").length;
+    return response;
+}
+
+btn.addEventListener('click', function(){
+    if (this.value == "Start") {
+        startTest();
+        typedWords.focus();
+    }
+    else if (this.value == "Done") {
+        typedWords.value = typedWords.value + "\n";
+        endTest();
+    }
+});
+
+typedWords.addEventListener('keyup', (event) => {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        endTest();
+    }
+});
